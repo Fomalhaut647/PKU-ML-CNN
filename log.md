46445c68fe8d8208c1de24bc0b508d362a191467
Fomalhaut <fomalhaut@stu.pku.edu.cn>
2025-11-21 15:39:24 +0800

    Implement 4 layers CNN

--- log.md
+++ log.md
@@ -18,3 +18,26 @@ transforms.Compose(
 ~~~
 
 ![](./results/1.png)
+
+
+
+# 4 层 CNN
+
+Best Val Acc: 0.0034
+
+~~~python
+epochs = 10
+batch_size = 32
+lr = 1e-3
+
+transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+~~~
+
+![](./results/2.png)
--- model.py
+++ model.py
@@ -5,13 +5,48 @@ import torch.nn.functional as F
 
 class CNN(nn.Module):
     def __init__(self, num_classes=200):
-        super(CNN, self).__init__()
-        self.conv = nn.Conv2d(3, 16, kernel_size=3, padding=1, stride=2)
-        self.pool = nn.MaxPool2d(4, 4)
-        self.fc = nn.Linear(16 * 28 * 28, num_classes)
+        super().__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 14 * 14, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes),
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv(x)))
-        x = x.view(x.size(0), -1)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
         x = self.fc(x)
         return x
